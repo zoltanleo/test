35137849a5cdbd3761a8aee9c03d22531c735da2
Zoltanleo <leobronstain@gmail.com>
2025-03-22 15:04:12 +0300

    2025.03.22.1

---
+++ unit1.pas
@@ -0,0 +1,26 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
